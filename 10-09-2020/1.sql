0b56493cc8595345d4213845af29eed9dd9ca333
swetha2915 <69582640+swetha2915@users.noreply.github.com>
2020-09-14 18:12:52 +0530

    Add files via upload

---
+++ 10-09-2020/1.sql
@@ -0,0 +1,345 @@
+use  project;
+create table worldcity
+(
+city varchar(30),
+country varchar(30),
+continent varchar(30),
+latitude float(5),
+NorthSouth char(1),
+longitude float(5),
+EastWest char(1));
+insert into worldcity (city,country,continent,latitude,NorthSouth,longitude,EastWest) values ('Athens','Greece','Europe',37.59,'N',23.44,'E'),
+																					('Atlanta','UnitedStates','NorthAmerica',33.45,'N',84.23,'W'),
+                                                                                    ('Dallas','United States','North America',32.47,'N',96.47,'W'),
+                                                                                    ('Nashville','United States','North America',36.09,'N',86.46,'W'),
+                                                                                    ('Victoria','Canada','North America',48.25,'N',123.21,'W'),
+                                                                                    ('Peterborough','Canada','North America',44.18,'N',79.18,'W'),
+                                                                                    ('Vancouver','Canada','North America',49.18,'N',123.04,'W'),
+                                                                                    ('Toledo','United States','North America',41.39,'N',83.82,'W'),
+                                                                                    ('Warsaw','Poland','Europe',52.15,'N',21.00,'E'),
+                                                                                    ('Lima','Peru','South America',12.03,'S',77.03,'W'),
+                                                                                    ('Rio De Janeiro','Brazil','South America',22.43,'S',43.13,'W'),
+                                                                                    ('Santiago','Chile','South America',33.27,'S',70.40,'W'),
+                                                                                    ('Bogota','Colombia','South America',04.36,'N',74.05,'W'),
+                                                                                    ('Buenos Aires','Argentina','South America',34.36,'S',58.28,'W'),
+                                                                                    ('Quito','Ecuador','South America',00.13,'S',78.30,'W'),
+                                                                                    ('Caracas','Venezuela','South America',10.30,'N',66.56,'W'),
+                                                                                    ('Madras','India','Asia',28.36,'N',77.12,'E'),
+                                                                                    ('Bombay','India','Asia',18.58,'N',72.50,'E'),
+                                                                                    ('Manchester','England','Europe',51.30,'N',0.0,null),
+                                                                                    ('Moscow','Russia','Europe',55.45,'N',37.35,'E'),
+                                                                                    ('Paris','France','Europe',48.52,'N',2.20,'E'),
+                                                                                    ('Shenyang','China','Asia',41.48,'N',123.27,'E'),
+                                                                                    ('Cairo','Egypt','Africa',30.03,'N',31.15,'E'),
+                                                                                    ('Tripoli','Lybia','Africa',32.54,'N',13.11,'E'),
+                                                                                    ('Beijing','China','Asia',39.56,'N',116.24,'E'),
+                                                                                    ('Rome','Italy','Europe',41.54,'N',12.29,'E'),
+                                                                                    ('Tokyo','Japan','Asia',35.42,'N',139.46,'E'),
+                                                                                    ('Sydney','Australia','Australia',33.52,'S',151.13,'E'),
+                                                                                    ('Sparta','Greece','Europe',37.05,'N',22.27,'E'),
+                                                                                    ('Madrid','Spain','Europe',40.24,'N',3.41,'W');
+select * from worldcity;
+USE project;
+
+
+select distinct country from worldcity order by country;
+select city,country from worldcity where country like "R%";
+select city,country from worldcity where country like "%A";
+
+
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+
+
+	
+
+
+
+	
+
+
+	
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
+	
+
+
